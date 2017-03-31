2cde017449e5459aa7b1a81c9124c357795334e1
Kyle Strohbeck <kyles@callbox.com>
2017-03-30 22:23:56 -0500

    Add readme as long description to setup.py.

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+from os import path
 from setuptools import setup
+
+
+long_description = 'Idiomatic parser combinators, Python-style.'
+if path.exists('README.rst'):
+    long_description = open('README.rst').read()
 
 
 setup(
@@ -7,6 +13,7 @@ setup(
     author='Kyle Strohbeck',
     author_email='kstrohbeck@gmail.com',
     description='Idiomatic parser combinators, Python-style.',
+    long_description=long_description,
     url='https://github.com/kstrohbeck/persimmon',
     packages=[],
 )
