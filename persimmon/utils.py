c2cd173f214be2eca0d80ba43fb327fabe0e12b2
Kyle Strohbeck <kyles@callbox.com>
2017-03-30 23:00:24 -0500

    Add docstring for Zipper.

--- persimmon/utils.py
+++ persimmon/utils.py
@@ -1,4 +1,8 @@
 class Zipper:
+    """List-like data structure that maintains a position, allowing for
+    efficient lookup and modification at the cursor.
+    """
+
     def __init__(self):
         """Create a new, empty zipper."""
         self._items = []
