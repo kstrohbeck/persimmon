e744e147725379bb24b769d8ca588962c6d9452e
Kyle Strohbeck <kyles@callbox.com>
2017-03-30 21:57:09 -0500

    Create initial setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='persimmon',
+    version='0.0.1',
+    author='Kyle Strohbeck',
+    author_email='kstrohbeck@gmail.com',
+    description='Idiomatic parser combinators, Python-style.',
+    url='https://github.com/kstrohbeck/persimmon',
+    packages=[],
+)
